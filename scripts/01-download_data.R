8e8b85123c57cd054fe42ba0c9b1eb9a78a46467
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-09-25 03:40:25 -0400

    Redownloaded full raw dataset

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data "covid-19 cases in toronto" from Open Data Toronto
+# Purpose: Downloads and saves the raw data "Covid-19 cases in Toronto" from Open Data Toronto
 # Author: Yunkai Gu
-# Date: 22 September 2024
+# Date: 24 September 2024
 # Contact: kylie.gu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: None
@@ -9,29 +9,12 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
 
 
-#### Search for package ID of my selected data ####
-search_results <- search_packages("covid-19 cases in toronto")
-package_ID <- search_results$id
-
-
 #### Download data ####
-# get package
-package <- show_package(package_ID)
-
-# get all resources for this package
-resources <- list_package_resources(package_ID)
-
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
-datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
-
-# load the first datastore resource as a sample
-data <- filter(datastore_resources, row_number()==1) %>% 
-  get_resource()
+all_data <- read_csv("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/64b54586-6180-4485-83eb-81e8fae3b8fe/resource/fff4ee65-3527-43be-9a8a-cb9401377dbc/download/COVID19%20cases.csv")
 
 
 #### Save data ####
-write_csv(data, "data/raw_data/covid_raw_data.csv") 
+write_csv(all_data, "data/raw_data/covid_raw_data.csv")
