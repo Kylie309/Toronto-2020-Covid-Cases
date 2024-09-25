15fecb95615a5fc9226e45d588ef0b22b975fa89
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-09-25 19:19:03 -0400

    Updated data simulation script

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
-# Purpose: Simulates data
+# Purpose: Simulates data of COVID-19 cases
 # Author: Yunkai Gu
-# Date: 23 September 2024
+# Date: 25 September 2024
 # Contact: kylie.gu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: None
@@ -17,46 +17,63 @@ set.seed(304)
 
 # Define the start and end date
 start_date <- as.Date("2020-01-01")
-end_date <- as.Date("2020-11-30")
+end_date <- as.Date("2020-12-31")
 
-# Set the number of random dates you want to generate
-number_of_dates <- 32000
+# Set the number of random cases desired to generate
+# Equal to the approximate cleaned data size
+number_of_cases <- 63000
 
+# Simulate variables needed for each case
 simulated_data <-
   tibble(
-    dates = as.Date(
+    reported_date = as.Date(
       runif(
-        n = number_of_dates,
+        n = number_of_cases,
         min = as.numeric(start_date),
         max = as.numeric(end_date)
       ),
     ),
     outbreak_associated = sample(
       x = c("YES", "NO"),
-      size = 32000,
+      size = number_of_cases,
       replace = TRUE
     ),
     age_group = sample(
       x = c("19 and younger", "20 to 29 Years", "30 to 39 Years", 
             "40 to 49 Years", "50 to 59 Years", "60 to 69 Years", 
             "70 to 79 Years", "80 to 89 Years", "90 and older"),
-      size = 32000,
+      size = number_of_cases,
       replace = TRUE
     ),
     source_of_infection = sample(
       x = c("Close Contact", "Community", "Household Contact", "Travel",
             "Outbreaks, Congregate Settings", "Outbreaks, Healthcare Institutions",
-            "Outbreaks, Other Settings", "No Information"),
-      size = 32000,
+            "Outbreaks, Other Settings"),
+      size = number_of_cases,
       replace = TRUE
     ),
     client_gender = sample(
-      x = c("Female", "Male", "Non-Binary", "Transgender", "Other", "Unknown"),
-      size = 32000,
+      x = c("Female", "Male", "Non-Binary", "Transgender", "Other"),
+      size = number_of_cases,
+      replace = TRUE
+    ),
+    outcome = sample(
+      x = c("RESOLVED", "FATAL"),
+      size = number_of_cases,
       replace = TRUE
     )
   )
 
+# Extract the year, month, and day from reported_date columns
+cleaned_simulated_data <-
+  simulated_data |>
+  mutate(
+    reported_year = year(reported_date),
+    reported_month = month(reported_date),
+    reported_day = day(reported_date)
+  ) |>
+  select(-reported_date)
+  
 
 #### Write_csv
-write_csv(simulated_data, file = "data/raw_data/simulated_covid_data.csv")
+write_csv(cleaned_simulated_data, file = "data/raw_data/simulated_covid_data.csv")
