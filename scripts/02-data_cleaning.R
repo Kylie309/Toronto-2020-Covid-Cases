b23a08e8fdce416c12e1585ca5aef8ff8e3af2de
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-09-27 12:23:40 -0400

    Modify styling slightly by styler

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -20,11 +20,11 @@ all_raw_data <- read_csv("data/raw_data/covid_raw_data.csv")
 all_cleaned_data <-
   all_raw_data |>
   # Simplify column names by standardizing them
-  janitor::clean_names() |> 
+  janitor::clean_names() |>
   # Extract the year from reported_date columns
   mutate(
     reported_year = year(reported_date),
-  ) |> 
+  ) |>
   # Remove rows where classification equals "probable"
   filter(classification != "probable") |>
   # Filters rows where reported_year equals 2020
@@ -33,11 +33,11 @@ all_cleaned_data <-
   select(age_group, source_of_infection, client_gender) |>
   # Remove rows that lack information in all three columns
   filter(!is.na(age_group) |
-           source_of_infection != "No Information" |
-           client_gender != "UNKNOWN") |>
+    source_of_infection != "No Information" |
+    client_gender != "UNKNOWN") |>
   # Rewrite the client_gender equals "TRANSWOMAN" to "TRANSGENDER"
   mutate(client_gender = ifelse(client_gender == "TRANS WOMAN", "TRANSGENDER", client_gender))
-  
+
 
 #### Save data ####
 write_csv(all_cleaned_data, "data/analysis_data/covid_analysis_data.csv")
