4830915c72b7548132b07c2f9fbd8e8bf0ffbcb5
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-09-27 04:49:00 -0400

    Updated data cleaning script

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,7 +1,7 @@
 #### Preamble ####
-# Purpose: Cleans the raw COVID data into an analysis dataset
+# Purpose: Cleans the raw COVID dataset into an analysis dataset
 # Author: Yunkai Gu
-# Date: 25 September 2024
+# Date: 27 September 2024
 # Contact: kylie.gu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Need to have downloaded the data
@@ -17,32 +17,27 @@ library(lubridate)
 #### Clean data ####
 all_raw_data <- read_csv("data/raw_data/covid_raw_data.csv")
 
-
-# Simplify column names by standardizing them
-# Extract the year, month, and day from reported_date columns
-# Filters rows where reported_year equals 2020
-# Remove rows where classification equals "probable"
-# Remove rows without information in all three columns:
-#   age_group, source_of_infection and client_gender 
-# Select only the columns that are necessary for data analysis
-
 all_cleaned_data <-
   all_raw_data |>
+  # Simplify column names by standardizing them
   janitor::clean_names() |> 
+  # Extract the year from reported_date columns
   mutate(
     reported_year = year(reported_date),
-    reported_month = month(reported_date),
-    reported_day = day(reported_date)
   ) |> 
+  # Remove rows where classification equals "probable"
+  filter(classification != "probable") |>
+  # Filters rows where reported_year equals 2020
   filter(reported_year == 2020) |>
-  filter(classification != "probable") |> 
-  filter(!is.na(outbreak_associated)) |>
+  # Select only the columns that are necessary for data analysis
+  select(age_group, source_of_infection, client_gender) |>
+  # Remove rows that lack information in all three columns
   filter(!is.na(age_group) |
            source_of_infection != "No Information" |
            client_gender != "UNKNOWN") |>
-  select(outbreak_associated, age_group, source_of_infection, client_gender, 
-         outcome, reported_year, reported_month, reported_day)
-
+  # Rewrite the client_gender equals "TRANSWOMAN" to "TRANSGENDER"
+  mutate(client_gender = ifelse(client_gender == "TRANS WOMAN", "TRANSGENDER", client_gender))
+  
 
 #### Save data ####
 write_csv(all_cleaned_data, "data/analysis_data/covid_analysis_data.csv")
